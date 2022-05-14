8124bb1e30a17db71a6d697f93200b10b4b812cc
Vstb02 <v.steblev02@gmail.com>
2022-05-14 19:36:56 +0300

    Добавил контекст базы данных

---
+++ src/Core/KetCRM.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using KetCRM.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetCRM.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Address> Addresses { get; set; }
+        DbSet<Department> Departments { get; set; }
+        DbSet<Group> Groups { get; set; }
+        DbSet<Parent> Parents { get; set; }
+        DbSet<Person> Persons { get; set; }
+        DbSet<Representative> Representatives { get; set; }
+        DbSet<SchoolType> SchoolTypes { get; set; }
+        DbSet<Specialization> specializations { get; set; }
+        DbSet<Student> Students { get; set; }
+        DbSet<Teacher> Teachers { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/Infrastructure/KetCRM.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using KetCRM.Application.Common.Interfaces;
+using KetCRM.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetCRM.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Address> Addresses { get; set; } = null!;
+        public DbSet<Department> Departments { get; set; } = null!;
+        public DbSet<Group> Groups { get; set; } = null!;
+        public DbSet<Parent> Parents { get; set; } = null!;
+        public DbSet<Person> Persons { get; set; } = null!;
+        public DbSet<Representative> Representatives { get; set; } = null!;
+        public DbSet<SchoolType> SchoolTypes { get; set; } = null!;
+        public DbSet<Specialization> specializations { get; set; } = null!;
+        public DbSet<Student> Students { get; set; } = null!;
+        public DbSet<Teacher> Teachers { get; set; } = null!;
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
