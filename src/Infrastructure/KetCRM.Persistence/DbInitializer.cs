d08f5026533c2922ea3ca45e068ade7dad24157a
Vstb02 <v.steblev02@gmail.com>
2022-05-14 19:38:26 +0300

    Добавил инициализатор бд

---
+++ src/Infrastructure/KetCRM.Persistence/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using KetCRM.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetCRM.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
