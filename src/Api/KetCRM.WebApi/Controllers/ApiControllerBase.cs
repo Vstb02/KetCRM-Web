e03a33f1ff5eab72e26c68aad499a2e6f81129fd
Vstb02 <v.steblev02@gmail.com>
2022-05-15 15:02:20 +0300

    Добавил контроллер person

---
+++ src/Api/KetCRM.WebApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KetCRM.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+    }
+}
---
+++ src/Api/KetCRM.WebApi/Controllers/Persons/PersonController.cs
@@ -0,0 +1,50 @@
+﻿using KetCRM.Application.Common.Interfaces.Persons;
+using KetCRM.Application.Models.Persons;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KetCRM.WebApi.Controllers.Persons
+{
+    public class PersonController : ApiControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        /// <summary>
+        /// Создание нового пользователя
+        /// </summary>
+        /// <param name="createPerson"></param>
+        /// <returns>Возвращает id созданного объекта Person</returns>
+        [HttpPost("CreatePerson")]
+        public async Task<IActionResult> CreatePerson([FromBody]CreatePersonDto createPerson)
+        {
+            try
+            {
+                var result = await _personService.CreatePerson(createPerson);
+
+                return Ok(result);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpDelete("DeletePerson/{Id}")]
+        public async Task<IActionResult> DeletePerson(Guid Id)
+        {
+            try
+            {
+                var result = await _personService.DeletePerson(Id);
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
